754dd79cea3a2f55e1ead5a858c975e0359cccd6
OnurNigiz <onurnigiz@hotmail.com>
2022-12-17 02:03:18 +0300

    seg fault (dumped core)

---
+++ ft_strlcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: onigiz <onigiz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 01:29:25 by onigiz            #+#    #+#             */
+/*   Updated: 2022/12/17 01:29:29 by onigiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s);
+
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srcsize;
+	size_t	i;
+
+	srcsize = ft_strlen(src);
+	i = 0;
+	if (dstsize != 0)
+	{
+		while (src[i] && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (srcsize);
+}
+
+int main()
+{
+    char *dst = "Onur";
+    const char *src = "Birsu";
+
+    printf("\nft_strlcpy: %ld", ft_strlcpy(dst, src, 5));
+    
+
+    return (0);
+}
