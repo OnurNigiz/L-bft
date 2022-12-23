6ac454e46d82187c9e7bd39b4cf9195f52340962
OnurNigiz <onurnigiz@hotmail.com>
2022-12-23 11:59:58 +0300

    strlcat update

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s);
+
+size_t ft_strlcat(char *dest, const char *src, size_t size)
+{
+    size_t i;
+    size_t dsize;
+
+    dsize = 0;
+    while (dest[dsize] != '\0' && dsize < size)
+        dsize++;
+    i = dsize;
+    while (src[dsize - i] && dsize + 1 < size)
+    {
+        dest[dsize] = src[dsize - i];
+        dsize++;
+    }
+    if (i < size)
+        dest[dsize] = '\0';
+    return(i + ft_strlen(src));
+}
+
+int main(void)
+{
+    char *dest = "Onur";    
+    const char *src = "Birsu";
+
+    printf("\nstrlcat: %ld\n\n", ft_strlcat(dest, src, 5));
+}
