0a8bc84a4bb7c6ea5734ff9f2eb1162cc5a1e3fb
OnurNigiz <onurnigiz@hotmail.com>
2023-01-02 14:23:14 +0300

    ends of mems' and bzero

--- ft_memmove.c
+++ ft_memmove.c
@@ -9,3 +9,38 @@
 /*   Updated: 2022/12/30 14:26:23 by onigiz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "ft_memcpy.c"
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+void *ft_memmove(void *dst, const void *src, size_t len)
+{
+    char *dest;
+    char *src2;
+    size_t i;
+
+    i = 0;
+    dest = (char *)dst;
+    src2 = (char *)src;
+    if (!dst && !src)
+        return (NULL);
+    if (src2 < dest)
+        while (i++ <= len)
+            dest[len - i] = src2[len - i];
+    else 
+        return (ft_memcpy(dst, src, len));
+    return (dst);
+}
+
+int main(void)
+{
+    char *src = "Birsu";
+    char *dst = "Onur";
+    int len = 5;
+
+    printf("%s\n", ft_memmove(dst, src, len));
+    
+    return (0);
+}
